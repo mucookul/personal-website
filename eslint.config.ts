a8c9fc2379d36aecf0de2c2cb780c9b76b15b201
mucookul <mucookul@gmail.com>
2026-06-10 23:34:41 -0400

    chore: updates

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,24 +3,75 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 
 import eslint from "@eslint/js";
-import { defineConfig, globalIgnores } from "eslint/config";
+import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
 
-import eslintPluginAstro from "eslint-plugin-astro";
-import eslintConfigPrettier from "eslint-config-prettier/flat";
+import { tailwind4 } from "tailwind-csstree";
 
-export default defineConfig(
-    eslint.configs.recommended,
-    tseslint.configs.strictTypeChecked,
-    tseslint.configs.stylisticTypeChecked,
-    ...eslintPluginAstro.configs.recommended,
-    eslintConfigPrettier,
+import eslintPluginAstro from "eslint-plugin-astro";
+import css from "@eslint/css";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import eslintPluginBetterTailwindcss from "eslint-plugin-better-tailwindcss";
+
+export default defineConfig([
     {
+        settings: {
+            "better-tailwindcss": {
+                entryPoint: "src/styles/global.css",
+            },
+        },
+    },
+    {
+        files: ["**/*.css"],
+        language: "css/css",
+        plugins: { css },
+        languageOptions: {
+            customSyntax: tailwind4,
+            tolerant: true,
+        },
+        extends: ["css/recommended"],
+        rules: {
+            "css/use-baseline": "off", // I can't be bothered man
+        },
+    },
+    {
+        files: ["**/*.{ts,tsx,cts,mts}"],
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+            },
+        },
+        extends: [
+            eslint.configs.recommended,
+            tseslint.configs.strictTypeChecked,
+            tseslint.configs.stylisticTypeChecked,
+            eslintPluginBetterTailwindcss.configs.recommended,
+        ],
+    },
+    {
+        files: ["**/*.astro"],
         languageOptions: {
             parserOptions: {
                 project: true,
             },
         },
+        extends: [
+            eslint.configs.recommended,
+            tseslint.configs.strictTypeChecked,
+            tseslint.configs.stylisticTypeChecked,
+            ...eslintPluginAstro.configs.recommended,
+            eslintPluginBetterTailwindcss.configs.recommended,
+        ],
     },
-    globalIgnores([".astro/", "dist/"]),
-);
+    {
+        files: ["**/*.{ts,tsx,cts,mts,astro}"],
+        rules: {
+            "better-tailwindcss/enforce-consistent-line-wrapping": "off",
+            "better-tailwindcss/enforce-consistent-class-order": "off",
+        },
+    },
+    {
+        ignores: [".astro/**/*", "dist/**/*"],
+    },
+    eslintConfigPrettier,
+]);
